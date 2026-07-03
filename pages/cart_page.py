f1708f8f96acd64bc8bcbc2effb7e750e05a08e3
Adrian Ponce de Leon <adrian.poncedeleon@unosquare.com>
2026-07-03 12:40:43 -0400

    cart page first validations

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,6 +1,23 @@
 from pages.base_page import BasePage
 
 class CartPage(BasePage):
+
+    def __init__(self, page):
+        super().__init__(page)
+        self.title = self.page.locator("[data-test=\"title\"]")
+        self.products = self.page.locator(".cart_item")
     
     def is_loaded(self):
-        return (self.page.locator("data-test:'title'").inner_text() == "Your Cart")
+        return (self.title.inner_text() == "Your Cart")
+
+    def find_product(self, product_name):
+        for i in range(self.products.count()):
+            product = self.products.nth(i).locator(".inventory_item_name").inner_text()
+            if product == product_name:
+                return self.products.nth(i)
+            
+    def product_exists(self, product_name):
+        if not self.find_product(product_name):
+            return False
+        else:
+            return True
---
+++ tests/test_cart.py
@@ -0,0 +1,8 @@
+def test_product_added_to_cart(login_page):
+    login_page.navigate()
+    inventory_page = login_page.login("standard_user", "secret_sauce")
+    inventory_page.add_to_cart("Sauce Labs Bolt T-Shirt")
+    cart_page = inventory_page.go_to_cart()
+
+    assert cart_page.is_loaded()
+    assert cart_page.product_exists("AAASauce Labs Bolt T-Shirt")
